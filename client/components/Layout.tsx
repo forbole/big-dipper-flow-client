0e3f01629ac95703955ee8b8cae88719bdd8e557
Kwun Yeung <kwun@forbole.com>
2020-08-06 03:39:08 +0800

    add top menu items

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import { Hidden, Container, Box, Typography, AppBar, IconButton, Menu, MenuItem, Toolbar} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,29 +13,43 @@ const useStyles = makeStyles((theme: Theme) =>
       marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
       color: theme.palette.common.black
     },
+    titleSM: {
+      flexGrow: 1,
+    },
     appBar: {
-        backgroundColor: theme.palette.common.white
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.black,
+        display: "flex",
+        textAlign: "left"
     }
   }),
 );
 
 const Layout = (props: { children: React.ReactNode; }) => {
     const classes = useStyles();
+    const theme = useTheme();
+    const smMatches = useMediaQuery(theme.breakpoints.down('xs'));
     
     return (
         <React.Fragment>
             <AppBar position="static" className={classes.appBar}>
                 <Toolbar>
-                    <Typography variant="h5" component="h1" className={classes.title}>
+                    <Box pr={2}>
+                    <Typography variant="h5" component="h1" className={(smMatches)?`${classes.title} ${classes.titleSM}`:classes.title}>
                         <img src="/img/flow-logo.svg" />
                     </Typography>
-                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                    </IconButton> */}
-                    
+                    </Box>
+                    <Hidden xsDown>
+                      <MenuItem>Blocks</MenuItem>
+                      <MenuItem>Activities</MenuItem>
+                    </Hidden>
+                    <Hidden smUp>
+                      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                      </IconButton>
+                    </Hidden>
                 </Toolbar>
             </AppBar>
             <Container maxWidth={false}>
