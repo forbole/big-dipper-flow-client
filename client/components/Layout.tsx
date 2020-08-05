01a01283777ad5a5a1c2d584b504926952bb33d9
Kwun Yeung <kwun@forbole.com>
2020-08-06 04:07:07 +0800

    add mobile menu

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import { Hidden, Container, Box, Typography, AppBar, IconButton, Menu, MenuItem, Toolbar} from '@material-ui/core';
+import { Hidden, Container, Box, Typography, AppBar, IconButton, Drawer, MenuItem, Toolbar} from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -18,11 +24,12 @@ const useStyles = makeStyles((theme: Theme) =>
     titleSM: {
       flexGrow: 1,
     },
+    mobileMenu: {
+      width: 250,
+    },
     appBar: {
         backgroundColor: theme.palette.common.white,
         color: theme.palette.common.black,
-        display: "flex",
-        textAlign: "left"
     }
   }),
 );
@@ -32,12 +39,33 @@ const Layout = (props: { children: React.ReactNode; }) => {
     const theme = useTheme();
     const smMatches = useMediaQuery(theme.breakpoints.down('xs'));
     
+    const [drawerState, setState] = React.useState({
+      top: false,
+      left: false,
+      bottom: false,
+      right: false,
+    });
+
+    const toggleDrawer = (open: boolean) => (
+      event: React.KeyboardEvent | React.MouseEvent,
+    ) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+  
+      setState({ ...drawerState, right: open });
+    };
+
     return (
         <React.Fragment>
             <AppBar position="static" className={classes.appBar}>
                 <Toolbar>
-                    <Box pr={2}>
-                    <Typography variant="h5" component="h1" className={(smMatches)?`${classes.title} ${classes.titleSM}`:classes.title}>
+                    <Box pr={2} className={(smMatches)?`${classes.title} ${classes.titleSM}`:classes.title}>
+                    <Typography variant="h5" component="h1">
                         <img src="/img/flow-logo.svg" />
                     </Typography>
                     </Box>
@@ -47,8 +75,26 @@ const Layout = (props: { children: React.ReactNode; }) => {
                     </Hidden>
                     <Hidden smUp>
                       <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
+                        <MenuIcon onClick={toggleDrawer(true)}/>
                       </IconButton>
+                      <Drawer anchor='right' open={drawerState['right']} onClose={toggleDrawer(false)}>
+                        <div className={classes.mobileMenu}>
+                          <List component="nav" aria-label="main mailbox folders">
+                            <ListItem button>
+                              {/* <ListItemIcon>
+                                <InboxIcon />
+                              </ListItemIcon> */}
+                              <ListItemText primary="Blocks" />
+                            </ListItem>
+                            <ListItem button>
+                              {/* <ListItemIcon>
+                                <DraftsIcon />
+                              </ListItemIcon> */}
+                              <ListItemText primary="Activities" />
+                            </ListItem>
+                          </List>
+                        </div>
+                      </Drawer>
                     </Hidden>
                 </Toolbar>
             </AppBar>
