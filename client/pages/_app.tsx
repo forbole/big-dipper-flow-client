e2cc92e64823155b3cd3eb7dbff8df0877017c64
Kwun Yeung <kwun@forbole.com>
2020-08-06 02:33:39 +0800

    update component case

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Layout from '../components/Layout'
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../themes/Theme';
+import Theme from '../themes/Theme';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -23,7 +23,7 @@ export default function MyApp(props) {
         <title>Big Dipper for Flow</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={Theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Layout>
