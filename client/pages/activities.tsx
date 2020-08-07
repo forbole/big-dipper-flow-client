94fa8451d8eed14e87a8e8e07bdcd900182ace97
Kwun Yeung <kwun@forbole.com>
2020-08-07 20:47:51 +0800

    add activities page

---
+++ client/pages/activities.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import { Paper, Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
+
+export default function Index() {
+    return (    
+        <React.Fragment>
+            <Box my={2}>
+                <Typography variant="h4" component="h1">
+                    Activities
+                </Typography>
+            </Box>  
+        </React.Fragment>
+    )
+}
