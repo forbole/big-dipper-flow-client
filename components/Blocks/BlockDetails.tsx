10acd83cb6a2638249436dd9176e0040f98d5ad2
Kwun Yeung <kwun@forbole.com>
2020-08-28 03:44:29 +0800

    put block details in tabs

--- components/Blocks/BlockDetails.tsx
+++ components/Blocks/BlockDetails.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
-import { Paper, Box, Typography, Accordion, AccordionSummary, AccordionDetails, Table, TableBody, TableCell, TableContainer, TableRow} from '@material-ui/core'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Paper, Box, Typography, Table, TableBody, TableCell, TableContainer, TableRow} from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import FolderSpecialIcon from '@material-ui/icons/FolderSpecial'
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'
+import { loadCSS } from 'fg-loadcss'
+import Icon from '@material-ui/core/Icon';
 import { BLOCK_BY_HEIGHT, BLOCK_BY_ID } from '../../queries/blocks'
 import { useQuery } from '@apollo/client'
 import numbro from 'numbro'
@@ -10,6 +16,39 @@ import utils from '../../utils'
 
 moment.relativeTimeThreshold('s', 60)
 moment.relativeTimeThreshold('ss', 1)
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: any;
+    value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`scrollable-force-tabpanel-${index}`}
+        aria-labelledby={`scrollable-force-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+}
+
+function a11yProps(index: any) {
+    return {
+      id: `scrollable-force-tab-${index}`,
+      'aria-controls': `scrollable-force-tabpanel-${index}`,
+    };
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,6 +71,24 @@ type BlockProps = {param: string}
 
 export const BlockDetails = ({param}:BlockProps) => {
     const classes = useStyles();
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+      setValue(newValue);
+    };
+
+    React.useEffect(() => {
+        const node = loadCSS(
+          'https://use.fontawesome.com/releases/v5.12.0/css/all.css',
+          document.querySelector('#font-awesome-css'),
+        );
+    
+        return () => {
+          node.parentNode!.removeChild(node);
+        };
+    }, []);
+
 
     let QUERY:import("graphql").DocumentNode
     let VARIABLES: {height?:number, id?:string}
@@ -56,73 +113,62 @@ export const BlockDetails = ({param}:BlockProps) => {
     const timestamp = moment.unix(parseFloat(`${block.timestamp.seconds}.${block.timestamp.nanos}`))
     return <React.Fragment>
         <Box mb={2}>
-        <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-            <TableBody>
-                <TableRow>
-                    <TableCell component="th">Height</TableCell>
-                    <TableCell>{numbro(block.height).format({thousandSeparated:true})}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell component="th">Timestamp (UTC)</TableCell>
-                    <TableCell>{timestamp.utc().format("dddd, MMMM Do YYYY, h:mm:ss a")} ({timestamp.utc().fromNow()})</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell component="th">ID</TableCell>
-                    <TableCell className="monospace">{utils.base64ToHex(block.id)}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell component="th">Parent ID</TableCell>
-                    <TableCell className="monospace">{utils.base64ToHex(block.parentId)}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell component="th">Block Time</TableCell>
-                    <TableCell>{block.blockTime} seconds</TableCell>
-                </TableRow>
-            </TableBody>
-            </Table>
-        </TableContainer>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                <TableBody>
+                    <TableRow>
+                        <TableCell component="th">Height</TableCell>
+                        <TableCell>{numbro(block.height).format({thousandSeparated:true})}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th">Timestamp (UTC)</TableCell>
+                        <TableCell>{timestamp.utc().format("dddd, MMMM Do YYYY, h:mm:ss a")} ({timestamp.utc().fromNow()})</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th">ID</TableCell>
+                        <TableCell className="monospace">{utils.base64ToHex(block.id)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th">Parent ID</TableCell>
+                        <TableCell className="monospace">{utils.base64ToHex(block.parentId)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th">Block Time</TableCell>
+                        <TableCell>{block.blockTime} seconds</TableCell>
+                    </TableRow>
+                </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-            >
-                <Typography className={classes.heading}>Signatures</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
+        <Paper>
+            <AppBar position="static" color="inherit" elevation={1}>
+                <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons="on"
+                indicatorColor="primary"
+                textColor="primary"
+                aria-label="scrollable force tabs example"
+                >
+                <Tab label={`Collections (${block.collections.length})`} icon={<FolderSpecialIcon />} {...a11yProps(0)} />
+                <Tab label={`Block Seals (${block.blockSeals.length})`} icon={<VerifiedUserIcon />} {...a11yProps(1)} />
+                <Tab label="Signatures" icon={<Icon className="fas fa-file-signature" />} {...a11yProps(2)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                
+            </TabPanel>
+            <TabPanel value={value} index={2}>
                 <Paper variant="outlined" square className={`${classes.well} monospace`}>
                     <Typography variant="caption">
                     {block.signatures}
                     </Typography>
                 </Paper>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-            >
-                <Typography className={classes.heading}>Block Seals ({block.blockSeals.length})</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Paper variant="outlined" square className={`${classes.well} monospace`}>
-                    <Typography variant="caption">
-
-                    </Typography>
-                </Paper>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion disabled={block.collections.length == 0}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3a-content"
-                id="panel3a-header"
-            >
-                <Typography className={classes.heading}>Collections ({block.collections.length})</Typography>
-            </AccordionSummary>
-        </Accordion>
+            </TabPanel>
+        </Paper>
     </React.Fragment>
 }
