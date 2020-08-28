f5f3fa0bbe93b07aa745ec9abcaab4381faee082
Kwun Yeung <kwun@forbole.com>
2020-08-28 18:52:24 +0800

    remove content loader

--- components/Loaders.tsx
+++ components/Loaders.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
-import ContentLoader from 'react-content-loader'
+import Skeleton from '@material-ui/lab/Skeleton';
 
-export const TableLoader = () => {
-    const random = Math.random() * (1 - 0.7) + 0.7
-    return (
-        <ContentLoader
-        viewBox="0 0 1060 40"
-        height={40}
-        width={1060}
-        speed={2}
-        >
-        <rect x="0" y="15" rx="4" ry="4" width="6" height="6.4" />
-        <rect x="34" y="13" rx="6" ry="6" width={200 * random} height="12" />
-        <rect x="633" y="13" rx="6" ry="6" width={23 * random} height="12" />
-        <rect x="653" y="13" rx="6" ry="6" width={78 * random} height="12" />
-        <rect x="755" y="13" rx="6" ry="6" width={117 * random} height="12" />
-        <rect x="938" y="13" rx="6" ry="6" width={83 * random} height="12" />
+type TableLoaderProps = { rows?: number }
 
-        <rect x="0" y="39" rx="6" ry="6" width="1060" height=".3" />
-        </ContentLoader>
-    )
+
+const TableRows = ({rows = 10}:TableLoaderProps) => {
+    let arrRows:JSX.Element[] = new Array<JSX.Element>();
+    for (let i = 0; i <rows; i++) {
+        arrRows.push(<Skeleton key={i} />)
+    }
+    return <>{arrRows}</>
+}
+
+export const TableLoader = ({rows = 10}:TableLoaderProps) => {
+    return <React.Fragment>
+        <TableRows rows={rows} />
+    </React.Fragment>
 }
