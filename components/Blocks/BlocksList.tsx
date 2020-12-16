4c94ccc46e048c503c6ff96e652c6906ecb74a82
Kwun Yeung <kwun@forbole.com>
2020-12-17 01:55:00 +0800

    remove semi-colon

--- components/Blocks/BlocksList.tsx
+++ components/Blocks/BlocksList.tsx
@@ -68,14 +68,14 @@ export const BlocksList = ({size, home = false}:ListProps) => {
 
     const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
         setPage(newPage);
-    };
+    }
     
     const handleChangeRowsPerPage = (
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     ) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
-    };
+    }
 
     return (
         <React.Fragment>
