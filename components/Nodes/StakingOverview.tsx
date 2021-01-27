2e658a4317d1127a5523619e9cd76511da8b82fb
Kwun Yeung <kwun@forbole.com>
2021-01-28 05:13:51 +0800

    Kwun/update supply (#54)
    * add supply to header
    
    * staking overview

--- components/Nodes/StakingOverview.tsx
+++ components/Nodes/StakingOverview.tsx
@@ -1,3 +1,74 @@
 import React from 'react'
-import { Paper, Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
+import { useQuery } from '@apollo/client'
+import { Paper, Grid, Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { STAKING_OVERVIEW } from '../../queries/staking'
+import { NODE_COUNT } from '../../queries/nodes'
+import utils from '../../utils'
+import numbro from 'numbro'
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        element: {
+            padding: '1rem',
+            marginBottom: '1rem'
+        },
+        value: {
+            color: theme.palette.text.secondary
+        }
+  }),
+)
+
+export const StakingOverview = () => {
+    const classes = useStyles()
+
+    const { loading, error, data } = useQuery(STAKING_OVERVIEW)
+
+    const nodeCount = useQuery(NODE_COUNT)
+
+    if (loading || nodeCount.loading) return <div>...</div>
+    if (error || nodeCount.error) return <div>-</div>
+
+    return <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={3}>
+            <Paper className={classes.element}>
+                <Typography variant="h6" gutterBottom>
+                    Node Count
+                </Typography>
+                <Typography variant="body1" gutterBottom className={classes.value}>
+                    {nodeCount.data.node_aggregate.aggregate.count}
+                </Typography>
+            </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+            <Paper className={classes.element}>
+                <Typography variant="h6" gutterBottom>
+                    Node Commission
+                </Typography>
+                <Typography variant="body1" gutterBottom className={classes.value}>
+                    {numbro(data.cutPercentage).format({output: "percent", mantissa: 2})}
+                </Typography>
+            </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+            <Paper className={classes.element}>
+                <Typography variant="h6" gutterBottom>
+                    Staking Ratio
+                </Typography>
+                <Typography variant="body1" gutterBottom className={classes.value}>
+                    {numbro(data.totalStake/data.getSupply).format({output: "percent", mantissa: 2})} ({numbro(data.totalStake).format({thousandSeparated: true, mantissa: 8})} {utils.types.FLOW_DENOM})
+                </Typography>
+            </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+            <Paper className={classes.element}>
+                <Typography variant="h6" gutterBottom>
+                    Weekly Payout
+                </Typography>
+                <Typography variant="body1" gutterBottom className={classes.value}>
+                    {numbro(data.weeklyPayout).format({thousandSeparated: true, mantissa: 8})} {utils.types.FLOW_DENOM}
+                </Typography>
+            </Paper>
+        </Grid>
+    </Grid>
+}
--- components/TokenPrice.tsx
+++ components/TokenPrice.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { createStyles, fade, makeStyles, Theme, useTheme } from '@material-ui/core/styles'
-import { Paper, Container, Box, Typography, Grid } from '@material-ui/core'
+import { useQuery } from '@apollo/client';
+import { createStyles, fade, makeStyles, Theme } from '@material-ui/core/styles'
+import { Paper, Grid } from '@material-ui/core'
 import numbro from 'numbro'
 import { useTokePrice } from '../components/Context/TokenProvider'
+import { TOTAL_SUPPLY } from '../queries/token'
+import utils from '../utils'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -11,9 +13,6 @@ const useStyles = makeStyles((theme: Theme) =>
         paddingLeft: theme.spacing(3),
         backgroundColor: theme.palette.grey[50],
         marginBottom: theme.spacing(2)
-        // background: 'url(/img/icon-blocks@2x.png) left center no-repeat',
-        // flexDirection: 'row',
-        // alignItems: 'center'
       }
   }),
 )
@@ -23,22 +22,30 @@ const TokenPrice = () => {
 
     const token = useTokePrice()
 
+    const { loading, error, data } = useQuery(TOTAL_SUPPLY)
+
+    if (loading) return <div>...</div>
+    if (error) return <div>-</div>
+
     if (token.usd != ''){
 
         return <Paper className={classes.token}>
             <Grid container spacing={3}>
-                <Grid item xs={12} sm={6} md={3}>
+                <Grid item xs={12} sm={6} lg={2}>
                     <strong>Price:</strong> {numbro(token.usd).formatCurrency({ mantissa: 2 })}
                 </Grid>
-                <Grid item xs={12} sm={6} md={3}>
+                <Grid item xs={12} sm={6} lg={3}>
                     <strong>Market Cap:</strong> {numbro(token.usd_market_cap).formatCurrency({ thousandSeparated:true,mantissa: 2 })}
                 </Grid>
-                <Grid item xs={12} sm={6} md={3}>
+                <Grid item xs={12} sm={6} lg={2}>
                     <strong>24h Volume:</strong> {numbro(token.usd_24h_vol).formatCurrency({ thousandSeparated:true, mantissa: 2 })}
                 </Grid>
-                <Grid item xs={12} sm={6} md={3}>
+                <Grid item xs={12} sm={6} lg={2}>
                     <strong>24h Change:</strong> {numbro(token.usd_24h_change).format({ mantissa: 2 })}%
-                </Grid>    
+                </Grid>
+                <Grid item xs={12} sm={12} lg={3}>
+                    <strong>Supply:</strong> {numbro(data.getSupply).format({thousandSeparated: true, mantissa: 8})} {utils.types.FLOW_DENOM}
+                </Grid>      
             </Grid>
         </Paper>
     }
