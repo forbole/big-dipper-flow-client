acc187e8f600415c13b16980d61c0ce876cb4ff2
Kwun Yeung <kwun@forbole.com>
2021-04-09 19:08:55 +0800

    fix block tx count if transaction ids not exist (#57)

--- utils/blocks.ts
+++ utils/blocks.ts
@@ -3,7 +3,7 @@ const getTxCount = (collections:Array<{transactionIds:Array<string>}>):number =>
     let txCount:number = 0
     
     collections.forEach(col => {
-        txCount += col.transactionIds.length
+        txCount += col.transactionIds?col.transactionIds.length:0
     })
 
     return txCount
